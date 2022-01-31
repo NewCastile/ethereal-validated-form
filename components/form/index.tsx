1e88ff4010ee39c087db181ccbd319903ac1758b
New Castile <carlosaguanipa1@gmail.com>
2022-01-31 18:03:26 -0400

    styles passed as a CSSProperties object

--- components/form/index.tsx
+++ components/form/index.tsx
@@ -1,6 +1,6 @@
 /** @format */
 
-import { ReactElement } from "react"
+import { CSSProperties, ReactElement } from "react"
 import { CompletionState, Step } from "../../types/completion"
 import { useCompletionContext } from "../../hooks/useCompletion"
 import { Stack, Text } from "@chakra-ui/react"
@@ -31,7 +31,7 @@ export default function Form() {
 		.filter((option) => option.state === "NOT VALIDATED")
 		?.map((option) => option.component)[0]
 	return (
-		<Stack {...ContainerStyles}>
+		<Stack spacing={"30px"} style={ContainerStyles}>
 			<Text fontWeight={"bold"} fontSize={"1.5em"}>
 				Ethereal Validated Form
 			</Text>
@@ -43,16 +43,15 @@ export default function Form() {
 	)
 }
 
-const ContainerStyles = {
-	boxShadow: "1px 1px 20px rgba(0,0,0,40%)" as any,
-	borderRadius: "10px" as any,
-	bgColor: "white" as any,
-	padding: "2em" as any,
-	direction: "column" as any,
-	justifyContent: "center" as any,
-	alignItems: "center" as any,
-	spacing: "30px" as any,
-	width: "100%" as any,
-	margin: "1em" as any,
-	maxWidth: "450px" as any,
+const ContainerStyles: CSSProperties = {
+	boxShadow: "1px 1px 20px rgba(0,0,0,40%)",
+	borderRadius: "10px",
+	backgroundColor: "white",
+	padding: "2em",
+	flexDirection: "column",
+	justifyContent: "center",
+	alignItems: "center",
+	width: "100%",
+	margin: "1em",
+	maxWidth: "450px",
 }
